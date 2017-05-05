85b12795a6c7d6052d709bcb8b064cdf7d985900
Martian1 <Martian1@users.noreply.github.com>
2017-05-05 11:31:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ app.get('/', function (req, res) {
     fs.readFile('./client/index.html',function(error,data){
         if(error) {
             //错误，发生错误信息
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('500 - Server Error');
+            res.send('500 - Server Error');
         }else{
             //成功，发生文件，并设置类型为‘text/html’
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.end(data);
+            res.set('Content-Type', 'text/html');
+            res.send(data);
         }
     });
 });
